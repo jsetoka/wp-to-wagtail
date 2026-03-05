d0a10b77b2378de5f98bde96056f4e0dd5dcf4c4
Job Etokabeka <jobet@MacBook-Air-2.local>
2026-03-05 17:49:35 +0100

    Migration vers POSTGRES

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -11,6 +11,24 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
+     }
+}
+DATABASES = {
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": "wagtail",
+    "USER": "wagtail",
+    "PASSWORD": "wagtail",
+    "HOST": "wp-to-wagtail-db-1",
+    "PORT": "5432",
+  }
+}
+
+
 
 try:
     from .local import *
